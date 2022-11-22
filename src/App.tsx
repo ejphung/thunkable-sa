cac4431d644cabb44604d8401b1bbb383eb0151d
Elizabeth Phung <phung.ej@gmail.com>
2022-11-21 17:07:13 -0800

    add header and icon button to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import AddProjectButton from './components/buttons/AddProjectButton';
+import ThunkableHeader from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThunkableHeader />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginRight: '130px',
+      }}
+      >
+        <AddProjectButton />
+      </div>
     </div>
   );
 }
